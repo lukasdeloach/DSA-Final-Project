aff7e1ccc14585f7430ec2c54ef2ad8b626a4157
deloachl <deloac58@students.rowan.edu>
2022-12-12 11:29:22 -0500

    While loop bugs

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -17,7 +17,7 @@
 
 import java.lang.*;
 
-public class CheckOut<Customer> extends Queue<T> {
+public class CheckOut<T> extends Queue<T> {
 
     String name;
     
@@ -78,7 +78,7 @@ public class CheckOut<Customer> extends Queue<T> {
             for(int ind = 0; ind < numItems; ind++) {
                 // saved for readability
                 int normIndex = (front+ind)%items.length;
-                Customer cust = (Customer)items[normIndex];
+                Customer cust = (Customer) items[normIndex];
                 str.append("\n\tCustomer " + cust.getName() + " has " + cust.getItemAmount() +
                            " items in the shopping basket.");
             }
